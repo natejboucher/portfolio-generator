d972d9e5bf51deaaab7c62a5c037949eadfaa1d2
Nate Boucher <natejboucher@gmail.com>
2022-01-27 23:40:26 -0500

    added validation for required input

--- app.js
+++ app.js
@@ -22,12 +22,28 @@ const promptProject = portfolioData => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?'
+      message: 'What is the name of your project?',
+      validate: projectInput => {
+        if (projectInput) {
+          return true;
+        } else {
+          console.log('Please enter name of Project.');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('Please enter a description of your project.');
+          return false;
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -38,7 +54,15 @@ const promptProject = portfolioData => {
     {
       type: 'input',
       name: 'link',
-      message: 'Enter the Github link to your project. (Required)'
+      message: 'Enter the Github link to your project. (Required)',
+      validate: linkInput => {
+        if (linkInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub project link.');
+          return false;
+        }
+      }
     },
     {
       type: 'confirm',
@@ -68,12 +92,28 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username',
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub username');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
